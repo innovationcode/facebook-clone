50f2715af85c8998b561d452c13ea546e5767cfd
lajawanti <lajawanti.dhake@gmail.com>
2020-09-03 22:24:06 -0400

    Building sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header.js';
+import Sidebar from './components/sidebar/Sidebar.js';
 import './App.css';
 
 function App() {
@@ -7,6 +8,10 @@ function App() {
     <div className="app">
       {/* <h1> Facebook clone ........</h1> */}
       <Header />
+
+      <div className = "app-body">
+          <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SidebarRow from '../sidebarRow/SidebarRow.js';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div className = "sidebar">
+            <h4>Sidebar......</h4>
+            <SidebarRow />
+        </div>
+    )
+}
+
+export default Sidebar;
