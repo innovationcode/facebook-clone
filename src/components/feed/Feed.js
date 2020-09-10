f8419a85955134b56b054f7b815dcba17aacf382
lajawanti <lajawanti.dhake@gmail.com>
2020-09-09 23:02:17 -0400

    Post.js and post.css working

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StoryReel from '../storyReel/StoryReel.js'
 import MessageSender from '../messageSender/MessageSender.js';
+import Post from '../post/Post.js';
 
 import './Feed.css';
 
@@ -9,6 +10,30 @@ const Feed = () => {
         <div className = "feed">
             <StoryReel />
             <MessageSender />
+
+            <Post
+                profilePic = ""
+                message = "This is beautiful..."
+                timestamp = "timestamp"
+                username = "LI"
+                image = "https://i.pinimg.com/originals/b4/c2/d7/b4c2d7c9fb28e69bff7ec8a1e37b8b42.jpg"
+            />
+            
+            <Post
+                profilePic = ""
+                message = "This is beautiful..."
+                timestamp = "timestamp"
+                username = "LI"
+                image = "https://i.pinimg.com/originals/b4/c2/d7/b4c2d7c9fb28e69bff7ec8a1e37b8b42.jpg"
+            />
+            
+            <Post
+                profilePic = ""
+                message = "This is beautiful..."
+                timestamp = "timestamp"
+                username = "LI"
+                image = "https://i.pinimg.com/originals/b4/c2/d7/b4c2d7c9fb28e69bff7ec8a1e37b8b42.jpg"
+            />
         </div>
     )
 }
