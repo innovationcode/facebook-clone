1d9ceff19bc4bcf1184b175fedb02e9e24b7d2d4
lajawanti <lajawanti.dhake@gmail.com>
2020-09-10 23:06:04 -0400

    Widgets component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Header from './components/header/Header.js';
 import Sidebar from './components/sidebar/Sidebar.js';
 import Feed from './components/feed/Feed.js';
 import Widgets from './components/widgets/Widgets.js';
+import Login from './components/login/Login.js';
+
 import './App.css';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      {/* <h1> Facebook clone ........</h1> */}
-      <Header />
+      {!user ? 
+          ( <Login /> ) : 
+          ( <>
+            <Header />
 
-      <div className = "app-body">
-          <Sidebar />
-          <Feed />
-          <Widgets />
-      </div>
+            <div className = "app-body">
+                <Sidebar />
+                <Feed />
+                <Widgets />
+            </div>
+            </>
+          )
+      }
     </div>
   );
 }
