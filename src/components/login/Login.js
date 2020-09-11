23dd9a52fa5627d564cbd66ac7a54c7f9fda5261
lajawanti <lajawanti.dhake@gmail.com>
2020-09-10 23:20:59 -0400

    login.js and login.css halfway complete

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
 
     return (
         <div className = 'login'>
-            <div className = 'login__logo'>
+            <div className = 'login__logo' style = {{ height: "10px"}} >
                 <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png" alt = "facebook logo"/>
                 <img src = "https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt = '' />
             </div> 
